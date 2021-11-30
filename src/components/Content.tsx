6169910aa380e8755c55f123d9bbebe748a57434
CleytonSousa <cleytonsouza808@gmail.com>
2021-11-30 18:08:25 -0300

    Finalizando desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
+import React from "react"
+import { MovieCard } from "./MovieCard"
+
+export function Content(props: any) {
   // Complete aqui
+
+  const movies = props.movie
+
+
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie: { imdbID: React.Key | null | undefined; Title: string; Poster: string; Runtime: string; Ratings: { Value: string }[] })  => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
+import React, { useState } from "react";
+import { Button } from "./Button";
+
+export function SideBar(props: any) {
   // Complete aqui
+  const {genres} = props
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return(
+    <nav className="sidebar">
+    <span>
+      Watch<p>Me</p>
+    </span>
+
+    <div className="buttons-container">
+      {genres.map((genre: {id: number, title: string, name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family' }) => (
+        <Button
+          className={props.active}
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => props.click(genre.id)}
+          selected={selectedGenreId == genre.id ? true : false}
+        />
+      ))}
+    </div>
+  </nav>
+  )
 }
